356a5a5174ba1208ddfb1d7da56653d749691705
Adam Biśta <410788@student.agh.edu.pl>
2024-03-08 22:19:19 +0100

    feat: new weather-app project

--- lab02 restapi/server.js
+++ lab02 restapi/server.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = 3000;
 const { checkAuth } = require("./util/auth");
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -16,44 +17,53 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get('/not-found', (req, res) => {
-  const message = req.query.message || 'Page not found';
-  res.render('error', { message });
+app.get("/not-found", (req, res) => {
+  const message = req.query.message || "Page not found";
+  res.render("error", { message });
 });
 
 app.get("/login", (req, res) => {
   const { credentialsError, loginError } = req.query;
   res.render("login", {
     credentialsError: credentialsError || "",
-    loginError: loginError || ""
+    loginError: loginError || "",
   });
 });
-
 
 app.get("/signup", (req, res) => {
   const { emailError, passwordError } = req.query;
   res.render("signup", {
     emailError: emailError || "",
-    passwordError: passwordError || ""
+    passwordError: passwordError || "",
   });
 });
 
-
 app.post("/weather", checkAuth, async (req, res) => {
-  console.log(req.body);
-  const city = req.body.city;
-  const apiKey = "EN36E78G48HKJDM54PEM59AZG";
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(
-    city
-  )}?unitGroup=metric&key=${apiKey}&contentType=json`;
+  const { city, startDate, endDate } = req.body;
+  const apiKey = process.env.WEATHER_API_KEY || "your-api";
+
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${startDate}/${endDate}?unitGroup=metric&include=days&key=${apiKey}&contentType=json`;
 
   try {
     const response = await axios.get(url);
     const weatherData = response.data;
-    res.render("weather", { weather: weatherData });
+    const temperatures = weatherData.days.map((day) => day.temp);
+    const maxTemperatures = weatherData.days.map((day) => day.tempmax);
+    const minTemperatures = weatherData.days.map((day) => day.tempmin);
+    const averageTemp =
+      temperatures.reduce((acc, curr) => acc + curr, 0) / temperatures.length;
+    const maxTemp = Math.max(...maxTemperatures);
+    const minTemp = Math.min(...minTemperatures);
+    res.render("weather", {
+      city: weatherData.resolvedAddress,
+      averageTemp,
+      maxTemp,
+      minTemp,
+      days: weatherData.days,
+    });
   } catch (error) {
     console.error("Error fetching weather data:", error);
-    res.redirect('/not-found?message=City not found');
+    res.redirect("/not-found?message=City not found");
   }
 });
 
