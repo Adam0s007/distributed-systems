9b2adad82a37bed575c9c28bd92a237ac4c3935c
Adam Biśta <410788@student.agh.edu.pl>
2024-03-10 01:36:11 +0100

    feat: add location api

---
+++ lab02 restapi/api/my-location.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const axios = require('axios');
+
+async function fetchLocation() {
+    const my_key = process.env.IPINFO_API_KEY || "demo";
+  try {
+    const response = await axios.get(`https://ipinfo.io?token=${my_key}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching location:', error);
+    throw error;
+  }
+}
+fetchLocation();
+exports.fetchLocation = fetchLocation;
--- lab02 restapi/routes/weather.js
+++ lab02 restapi/routes/weather.js
@@ -1,39 +1,49 @@
 require("dotenv").config();
 const express = require("express");
 const { checkAuth } = require("../util/auth");
-const {validateWeatherRequest} = require("../util/validation")
-const {fetchWeatherData} = require("../api/fetchWeatherData");
-const {getStats} = require("../util/statistics");
-
+const { validateWeatherRequest } = require("../util/validation");
+const { fetchWeatherData } = require("../api/fetchWeatherData");
+const { getStats } = require("../util/statistics");
+const {fetchLocation} = require("../api/my-location");
 const router = express.Router();
 
-
 router.post("/weather", checkAuth, async (req, res) => {
-    let errMess = validateWeatherRequest(req);
+  let { city, startDate, endDate } = req.body;
     
-    if(errMess !== ''){
-      return res.redirect(`/not-found?message=${encodeURIComponent(errMess)}`);
+    if (!req.body.location) {
+        const errMess = validateWeatherRequest(req);
+        if (errMess !== "") {
+            return res.redirect(`/not-found?message=${encodeURIComponent(errMess)}`);
+        }
     }
-    const { city, startDate, endDate } = req.body;
-    console.log(city, startDate, endDate);
-    try {
-        const weatherData = await fetchWeatherData(city, startDate, endDate);
-        const statistics = getStats(weatherData);
-        res.render("weather", { 
-            city: weatherData.resolvedAddress,
-            cityOrigin:city,
-            startDate,
-            endDate,
-            days: weatherData.days,
-            statistics,
-        });
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-        res.status(404).redirect("/not-found?message=City not found");
+    
+    if (req.body.location) {
+        try {
+            const locationData = await fetchLocation();
+            city = locationData.city;
+            startDate = new Date().toISOString();
+            endDate = new Date(new Date().setDate(new Date().getDate() + 5)).toISOString(); // Add 5 days to current date
+        } catch (error) {
+            console.error('Error fetching location data:', error);
+            return res.redirect(`/not-found?message=${encodeURIComponent("Error fetching location data")}`);
+        }
     }
-  });
-  
+  console.log(city, startDate, endDate);
+  try {
+    const weatherData = await fetchWeatherData(city, startDate, endDate);
+    const statistics = getStats(weatherData);
+    res.render("weather", {
+      city: weatherData.resolvedAddress,
+      cityOrigin: city,
+      startDate,
+      endDate,
+      days: weatherData.days,
+      statistics,
+    });
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(404).redirect("/not-found?message=City not found");
+  }
+});
+
 module.exports = router;
-
-
-
